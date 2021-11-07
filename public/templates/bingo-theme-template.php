d194194abd8ad5a8cd3f044b56288b7032b8e36b
stripearmy <mkrtchyan.ars@gmail.com>
2021-11-07 22:42:02 +0400

    css upd html upd

--- public/templates/bingo-theme-template.php
+++ public/templates/bingo-theme-template.php
@@ -27,7 +27,7 @@ $data = get_post_meta($post->ID);
                     <span class="lbcg-sidebar-arrow"></span>
                 </div>
                 <div class="lbcg-sidebar-body">
-                    <a href="#" class="lbcg-sidebar-link">Bingo Card Generator</a>
+                    <a href="#" class="lbcg-sidebar-link active"><!-- active for active -->Bingo Card Generator</a>
                     <a href="#" class="lbcg-sidebar-link">1-75 Bingo</a>
                     <a href="#" class="lbcg-sidebar-link">1-90 Bingo</a>
                     <a href="#" class="lbcg-sidebar-link">Virtual Bingo</a>
@@ -60,9 +60,96 @@ $data = get_post_meta($post->ID);
                         <input class="lbcg-input" id="lbcg-title" type="text" value="<?php echo !empty($data['bingo_card_title'][0]) ? $data['bingo_card_title'][0] : ''; ?>" />
                     </div>
                     <div class="lbcg-input-wrap">
+                        <label for="lbcg-title" class="lbcg-label">Enter a Subtitle</label>
+                        <div class="lbcg-input-wrap-in lbcg-input-wrap--subtitle">
+                            <label class="lbcg-label lbcg-label--single">
+                                <input class="lbcg-input" id="lbcg-subtitle-1" type="text" value="" />
+                            </label>
+                            <label class="lbcg-label lbcg-label--single">
+                                <input class="lbcg-input" id="lbcg-subtitle-2" type="text" value="" />
+                            </label>
+                            <label class="lbcg-label lbcg-label--single">
+                                <input class="lbcg-input" id="lbcg-subtitle-3" type="text" value="" />
+                            </label>
+                            <label class="lbcg-label lbcg-label--single">
+                                <input class="lbcg-input" id="lbcg-subtitle-4" type="text" value="" />
+                            </label>
+                            <label class="lbcg-label lbcg-label--single">
+                                <input class="lbcg-input" id="lbcg-subtitle-5" type="text" value="" />
+                            </label>
+                        </div>
+                    </div>
+                    <div class="lbcg-input-wrap">
                         <label for="lbcg-body-content" class="lbcg-label">Enter word or numbers</label>
                         <textarea class="lbcg-input" id="lbcg-body-content" name="lbcg-body-content" cols="" rows="11"><?php echo !empty($data['bingo_card_content'][0]) ? $data['bingo_card_content'][0] : ''; ?></textarea>
                     </div>
+                    <div class="lbcg-input-wrap">
+                        <label for="lbcg-grid-size" class="lbcg-label">Select Grid Size</label>
+                        <select name="lbcg-grid-size" id="lbcg-grid-size" class="lbcg-select">
+                            <option value="grid-3x3" selected>3x3</option>
+                            <option value="grid-4x4">4x4</option>
+                            <option value="grid-5x5">5x5</option>
+                        </select>
+                    </div>
+                    <div class="lbcg-input-wrap">
+                        <label for="lbcg-font" class="lbcg-label">Select Font Family</label>
+                        <select name="lbcg-font" id="lbcg-font" class="lbcg-select">
+                            <option value="roboto" selected>Roboto</option>
+                            <option value="noto-sans">Noto Sans</option>
+                        </select>
+                    </div>
+                    <div class="lbcg-input-wrap">
+                        <input type="checkbox" class="lbcg-checkbox lbcg-checkbox--collapse" id="lbcg-free-space-check" hidden />
+                        <label for="lbcg-free-space-check" class="lbcg-checkbox-holder"></label>
+                        <label for="lbcg-free-space-check" class="lbcg-label">Include free space?</label>
+                        <div class="lbcg-input-wrap-in lbcg-input-wrap--collapse">
+                            <label class="lbcg-label">
+                                <input type="text" class="lbcg-input" id="lbcg-free-space" />
+                            </label>
+                        </div>
+                    </div>
+                    <div class="lbcg-input-wrap">
+                        <input type="checkbox" class="lbcg-checkbox lbcg-checkbox--collapse" id="lbcg-bg-image-check" hidden />
+                        <label for="lbcg-bg-image-check" class="lbcg-checkbox-holder"></label>
+                        <label for="lbcg-bg-image-check" class="lbcg-label">Background Image</label>
+                        <div class="lbcg-input-wrap-in lbcg-input-wrap--collapse">
+                            <label class="lbcg-label">
+                                <input type="file" class="lbcg-input" id="lbcg-bg-image" />
+                            </label>
+                            <label class="lbcg-label">
+                                <select name="lbcg-bg-pos" id="lbcg-bg-pos" class="lbcg-select">
+                                    <option value="0" disabled selected>Background Position</option>
+                                    <option value="top-left">Top Left</option>
+                                    <option value="top-center">Top Center</option>
+                                </select>
+                            </label>
+                            <label class="lbcg-label">
+                                <select name="lbcg-bg-rep" id="lbcg-bg-rep" class="lbcg-select">
+                                    <option value="0" disabled selected>Background Repeat</option>
+                                    <option value="repeat">Repeat</option>
+                                    <option value="repeat-x">Repeat X</option>
+                                    <option value="repeat-y">Repeat Y</option>
+                                    <option value="no-repeat-y">No Repeat</option>
+                                </select>
+                            </label>
+                            <label class="lbcg-label">
+                                <select name="lbcg-bg-size" id="lbcg-bg-size" class="lbcg-select">
+                                    <option value="0" disabled selected>Background Size</option>
+                                    <option value="cover">Cover</option>
+                                    <option value="contain">Contain</option>
+                                </select>
+                            </label>
+                        </div>
+                    </div>
+                    <div class="lbcg-input-wrap">
+                        <label for="lbcg-bg-color" class="lbcg-label">Background Color</label>
+                        COLOR PICKER HERE
+                    </div>
+                    <div class="lbcg-input-wrap">
+                        <label for="lbcg-bg-opacity" class="lbcg-label">Background Opacity</label>
+                        <input type="number" min="0" max="100" placeholder="0-100" class="lbcg-input" id="lbcg-bg-opacity" />
+                    </div>
+
                 </div>
             </div>
             <div class="lbcg-content-right">
@@ -74,6 +161,7 @@ $data = get_post_meta($post->ID);
 
                                 --lbcg-card-bg-color: #FFF;
                                 --lbcg-card-bg-image: url(https://thumbs.dreamstime.com/z/crazy-cat-tongue-hanging-out-40087599.jpg);
+                                --lbcg-card-bg-opacity: 1;
 
                                 /* header styles */
 
@@ -83,6 +171,7 @@ $data = get_post_meta($post->ID);
                                 --lbcg-card-header-text-color: #FFF;
                                 --lbcg-card-header-bg-color: transparent;
                                 --lbcg-card-header-bg-image: none;
+                                --lbcg-card-header-bg-opacity: 1;
 
                                 /* body styles */
 
@@ -94,10 +183,11 @@ $data = get_post_meta($post->ID);
                                 --lbcg-card-col-text-color: #79ffd3;
                                 --lbcg-card-col-bg-color: #003221;
                                 --lbcg-card-col-border-color: #45ffbf;
+                                --lbcg-card-col-bg-opacity: .5;
                             }
                         </style>
                         <div class="lbcg-card-header">
-                            <span class="lgbc-card-header-text">Make Your Own Bingo!</span>
+                            <span class="lbcg-card-header-text">Make Your Own Bingo!</span>
                         </div>
                         <div class="lbcg-card-body">
                             <div class="lbcg-card-body-grid lbcg-grid-5"><!-- lbcg-grid-3/lbcg-grid-4 -->
