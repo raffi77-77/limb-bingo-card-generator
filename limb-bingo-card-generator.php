d7b5a71d58e0ae96a5ecfe0608438143e2e78004
Vahan <vahan.grigoryan.1107@gmail.com>
2022-01-27 14:39:02 +0400

    Fixed main archve page view.

--- limb-bingo-card-generator.php
+++ limb-bingo-card-generator.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Limb Bingo Cards Generator
  * Description: Generate, share or print bingo cards
- * Version: 1.0.18
+ * Version: 1.0.21
  * Author: Raffi Yeghiazaryan
  * Author URI: https://www.upwork.com/freelancers/~01ff918a8b3fff85af
  * License: GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
@@ -12,7 +12,7 @@ function run_lbcg() {
 	$plugin_dir = plugin_dir_path( __FILE__ );
 	$plugin_url = plugin_dir_url( __FILE__ );
 	require_once $plugin_dir . 'includes/class-lbcg.php';
-	LBCG::get_instance( '1.0.18', $plugin_dir, $plugin_url );
+	LBCG::get_instance( '1.0.21', $plugin_dir, $plugin_url );
 }
 
 add_action( 'init', 'lbcg_register_custom_post_types', 0 );
